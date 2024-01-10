36331a73d48cf2897d9305987c2f0c5aa627ea5e
Dom-21 <130161618+Dom-21@users.noreply.github.com>
2024-01-10 06:29:04 +0530

    Update pgm6.py

--- P6/pgm6.py
+++ P6/pgm6.py
@@ -1,86 +1,24 @@
-import csv
-import random
-import math
-
-def loadcsv(filename):
-    dataset = list(csv.reader(open(filename,"r")))
-    for i in range(len(dataset)):
-        dataset[i] = [float(x) for x in dataset[i]]
-    return dataset
-
-def splitDataset(dataset, splitRatio):
-    trainSize = int(len(dataset) * splitRatio)
-    trainSet = []
-    trainSet,testSet = dataset[:trainSize],dataset[trainSize:]
-    return [trainSet, testSet]
-
-def mean(numbers):
-    return sum(numbers)/(len(numbers))
-
-def stdev(numbers):
-    avg = mean(numbers)
-    v = 0
-    for x in numbers:
-        v += (x-avg)**2
-    return math.sqrt(v/(len(numbers)-1))
-
-def summarizeByClass(dataset):
-    separated = {}
-    for i in range(len(dataset)):
-        vector = dataset[i]
-        if (vector[-1] not in separated):
-            separated[vector[-1]] = []
-        separated[vector[-1]].append(vector)
-    summaries = {}
-    for classValue, instances in separated.items():
-        summaries[classValue] = [(mean(attribute), stdev(attribute)) for attribute in zip(*instances)][:-1]
-    return summaries
-
-def calculateProbability(x, mean, stdev):
-    exponent = math.exp((-(x-mean)**2)/(2*(stdev**2)))
-    return (1 / math.sqrt(2*math.pi*(stdev**2))) * exponent
-
-def predict(summaries, inputVector):
-    probabilities = {}
-    for classValue, classSummaries in summaries.items():
-        probabilities[classValue] = 1
-        for i in range(len(classSummaries)):
-            mean, stdev = classSummaries[i]
-            x = inputVector[i]
-            probabilities[classValue] *= calculateProbability(x, mean, stdev)
-    bestLabel, bestProb = None, -1
-    for classValue, probability in probabilities.items():
-        if bestLabel is None or probability > bestProb:
-            bestProb = probability
-            bestLabel = classValue
-    return bestLabel
-
-def getPredictions(summaries, testSet):
-    predictions = []
-    for i in range(len(testSet)):
-        result = predict(summaries, testSet[i])
-        predictions.append(result)
-    return predictions
-
-def getAccuracy(testSet, predictions):
-    correct = 0
-    for i in range(len(testSet)):
-        if testSet[i][-1] == predictions[i]:
-            correct += 1
-    return (correct/(len(testSet))) * 100.0
-
-filename = 'diabetes2.csv'
-splitRatio = 0.9
-dataset = loadcsv(filename)
-actual = []
-trainingSet, testSet = splitDataset(dataset, splitRatio)
-for i in range(len(testSet)):
-	vector = testSet[i]
-	actual.append(vector[-1])
-print('Split {0} rows into train={1} and test={2} rows'.format(len(dataset), len(trainingSet), len(testSet)))
-summaries = summarizeByClass(trainingSet) #will have (mean,sd) for all attributes.(for class 1 & 0 separately)
-predictions = getPredictions(summaries, testSet)
-print('\nActual values:\n',actual)
-print("\nPredictions:\n",predictions)
-accuracy = getAccuracy(testSet, predictions)
-print("Accuracy",accuracy)
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
+df = pd.read_csv("pima_indian.csv")
+feature_col_names = ['num_preg', 'glucose_conc', 'diastolic_bp', 'thickness', 'insulin', 'bmi','diab_pred', 'age']
+predicted_class_names = ['diabetes']
+X = df[feature_col_names].values # these are factors for the prediction
+y = df[predicted_class_names].values # this is what we want to predict
+#splitting the dataset into train and test data
+xtrain,xtest,ytrain,ytest=train_test_split(X,y,test_size=0.33)
+print ('\n the total number of Training Data :',ytrain.shape)
+print ('\n the total number of Test Data :',ytest.shape)
+# Training Naive Bayes (NB) classifier on training data.
+clf = GaussianNB().fit(xtrain,ytrain.ravel())
+predicted = clf.predict(xtest)
+predictTestData= clf.predict([[6,148,72,35,0,33.6,0.627,50]])
+#printing Confusion matrix, accuracy, Precision and Recall
+print('\n Confusion matrix')
+print(metrics.confusion_matrix(ytest,predicted))
+print('\n Accuracy of the classifier is',metrics.accuracy_score(ytest,predicted))
+print('\n The value of Precision', metrics.precision_score(ytest,predicted))
+print('\n The value of Recall', metrics.recall_score(ytest,predicted))
+print("Predicted Value for individual Test Data:", predictTestData)
